102e41b606344a0501b796a02818ee9f24beed83
LD-UO <ldusa012@uottawa.ca>
2024-11-08 21:06:42 -0500

    working backend

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,42 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.UUID
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(
+    private val employeesRepository: EmployeesRepository,
+    private val mongoOperations: MongoOperations
+) {
+    @QueryMapping
+    fun employees(): List<Employee> {
+        return employeesRepository.findAll()
+    }
 
+    @MutationMapping
+    fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
+        if (input.name != null && input.dateOfBirth != null && input.city != null && input.salary != null) {
+            val employee = Employee(
+                name = input.name,
+                dateOfBirth = input.dateOfBirth,
+                city = input.city,
+                salary = input.salary,
+                gender = input.gender,
+                email = input.email
+            )
+            employee.id = UUID.randomUUID().toString()
+            employeesRepository.save(employee)
+            return employee
+        } else {
+            throw Exception("Invalid input: name, dateOfBirth, city, and salary are required fields.")
+        }
+    }
 }
